2d945fb28710c42ce53418cc9cb19206a0aaf506
root <root@DESKTOP-3RMCEU7>
2024-05-09 20:19:08 -0600

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,11 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "portalcrm60eaac8460eaac84": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
---
+++ src/views/SignUp/SignUp.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Amplify } from 'aws-amplify';
+import config from '../../aws-exports';
+Amplify.configure(config);
+import logo from "../../assets/react.svg";
+import "@aws-amplify/ui-react/styles.css";
+import {
+    withAuthenticator,
+    Button,
+    Heading,
+    Image,
+    View,
+    Card,
+} from "@aws-amplify/ui-react";
+
+
+function SignUp() {
+    return (
+        <>
+            <View className="App">
+                <Card>
+                    <Image src={logo} className="App-logo" alt="logo" />
+                    <Heading level={1}>We now have Auth!</Heading>
+                </Card>
+                <Button onClick={signOut}>Sign Out</Button>
+            </View>
+        </>
+    )
+}
+
+export default withAuthenticator(SignUp)
